c27893586bb80c3ce3087b30a485841be48157cb
devseed <devseed1@gmail.com>
2023-03-29 00:34:06 +0900

    nvl_asar.py v0.3, add AES_CFB decrypt method for _pixiMiddleware

--- src/nvl/nvl_asar.py
+++ src/nvl/nvl_asar.py
@@ -2,7 +2,7 @@
 
 """
 This is for decrypting asar file made by nvl cloud,
-v0.2, developed by devseed
+v0.3, developed by devseed
 
 Tested game: OurEndOfTheWorld
 """
@@ -13,7 +13,14 @@ import mmap
 import json
 import numpy as np
 from io import BytesIO
-from typing import List
+# pip install pycryptodome
+from Crypto.Cipher import AES
+from typing import List, Dict, Tuple
+
+# _pixiMiddleware, AES_CFB key iv from OurEndOfTheWorld, for other game, edit here
+USE_PIXIMID = True #  enable PIXIMID decrypt
+PIXIMID_KEY = bytes([142, 134, 122, 174, 139, 75, 85, 236, 1, 134, 58, 225, 136, 147, 59, 127])
+PIXIMID_IV = bytes([196, 132, 205, 125, 176, 20, 171, 182, 209, 64, 82, 130, 168, 238, 166, 236])
 
 class nvlasar_entry_t:
     def __init__(self, curdir, name, size, offset, hashstr) -> None:
@@ -35,6 +42,18 @@ def decrypt_nvlasar(data: bytes, hash_data: bytes, hash_offset: int,
         hash_offset += 1
         hash_offset %= len(hash_data)
 
+    return bufio
+
+def decrypt_nvlasarjpg(data: bytes, key: bytes, iv: bytes, 
+    bufio: BytesIO=None) -> BytesIO:
+    
+    """
+    decrypt with AES_CFB 
+    """
+    if bufio is None:
+        bufio = BytesIO(data) # block size is 16
+    cipher = AES.new(key, AES.MODE_CFB, iv, segment_size=128) 
+    bufio.write(cipher.decrypt(data))
     return bufio
 
 def decrypt_nvlasar_batch(data: bytes, hash_data: bytes, hash_offset: int, 
@@ -66,24 +85,21 @@ def decrypt_nvlasar_batch(data: bytes, hash_data: bytes, hash_offset: int,
 
     return bufio
 
-def export_nvlasar(inpath, outdir):
-
-    fd = os.open(inpath, os.O_RDWR)
-    m = mmap.mmap(fd, 0)
-
+def get_nvlasar_index(data: bytes) -> Tuple[bytes, dict]:
     # decrypt nvl asar index
     print("decrypting nvl asar index ...")
-    index_size = int.from_bytes(m[4:8], 'little', signed=False) - 8
-    index_data = decrypt_nvlasar_batch(m[0x10:0x10+index_size], m[4:8], 0).getbuffer()
+    index_size = int.from_bytes(data[4:8], 'little', signed=False) - 8
+    index_data = decrypt_nvlasar_batch(data[0x10:0x10+index_size], data[4:8], 0).getbuffer()
     index_data = bytes(index_data)
     index_end = index_data.rfind(b'\0')
     index_json = json.loads(index_data[:index_end].decode())
-    with open(os.path.join(outdir, "nvlasar_index.json"), "w") as fp:
-        json.dump(index_json, fp, indent=2)
+    return index_data, index_json
 
+def get_nvlasar_list(index_json: json) -> List[nvlasar_entry_t]:
+    
     # parse nvl asar index
     print("parsing nvl asar content ...")
-    filestack = [{"obj": index_json, "objdir": outdir}]
+    filestack = [{"obj": index_json, "objdir": ""}]
     nvlasar_entries: List[nvlasar_entry_t] = []
     while len(filestack) > 0:
         _t = filestack.pop()
@@ -98,40 +114,91 @@ def export_nvlasar(inpath, outdir):
                 else:
                     nvlasar_entries.append(nvlasar_entry_t(
                         objdir, name,  int(v['size']), int(v["offset"]), v["hash"]))
+    return nvlasar_entries
 
+def get_nvlasar_hashmap(data: bytes, content_offset: int, 
+    nvlasar_entries: List[nvlasar_entry_t]) -> Dict[str, str]:
+    
+    # find assets.json
+    nvlasar_map = dict()
+    for entry in nvlasar_entries:
+        if entry.name == "assets.json":
+                start = content_offset + entry.offset
+                hashdata = entry.hashstr.encode() + \
+                    int.to_bytes(entry.size, 4, 'little', signed=False)
+                mapdata = decrypt_nvlasar_batch(data[start: start + entry.size], 
+                    hashdata, entry.size % len(hashdata)).getbuffer()
+                asset_json = json.loads(bytes(mapdata).decode())
+                break
+
+    if asset_json:
+        for k, v in asset_json.items():
+            nvlasar_map.update({v: k})
+
+    return nvlasar_map
+
+def dump_nvlasar_entries(data: bytes, content_offset: int, 
+    nvlasar_entries: List[nvlasar_entry_t], 
+    nvlasar_map: Dict[str, str], outdir: str):
+    
     # decrypt nvl asar content
-    content_start = index_size + 0x10
     for i, entry in enumerate(nvlasar_entries):
-        entry_path = os.path.join(entry.curdir, entry.name)
-        print(f"{i+1}/{len(nvlasar_entries)} {entry_path} "
+        if entry.name in nvlasar_map:
+            path = os.path.join(outdir, nvlasar_map[entry.name])
+            path = path.replace('/', os.path.sep)
+        else: 
+            path = os.path.join(outdir, entry.curdir, entry.name)
+        targetdir = os.path.dirname(path)
+        if not os.path.exists(targetdir): os.makedirs(targetdir)
+        
+        print(f"{i+1}/{len(nvlasar_entries)} {path} "
             f"size={entry.size:x} offset={entry.offset:x} hash={entry.hashstr}")
-        path = os.path.join(outdir, entry.curdir)
-        if not os.path.exists(path): os.makedirs(path)
-        path = os.path.join(path, entry.name)
+
         with open(path, "wb") as fp:
-            start = content_start + entry.offset
+            item_offset = content_offset + entry.offset
             hashdata = entry.hashstr.encode() + \
                 int.to_bytes(entry.size, 4, 'little', signed=False)
-            decrypt_nvlasar_batch(m[start: start + entry.size], 
-                hashdata, entry.size % len(hashdata),fp)
+            extname = os.path.splitext(path)[1].lower()
+            if  extname not in {".jpg", ".png"} or not USE_PIXIMID: 
+                decrypt_nvlasar_batch(data[item_offset: item_offset + entry.size], 
+                    hashdata, entry.size % len(hashdata), fp)
+            else:  #  use pixiMiddleware AES_CFB
+                _dataio = decrypt_nvlasar_batch(data[item_offset: item_offset + entry.size], 
+                    hashdata, entry.size % len(hashdata))
+                decrypt_nvlasarjpg(_dataio.getbuffer(), PIXIMID_KEY, PIXIMID_IV, fp)
+
+def export_nvlasar(inpath, outdir):
+
+    fd = os.open(inpath, os.O_RDWR)
+    m = mmap.mmap(fd, 0)
+
+    index_data, index_json = get_nvlasar_index(m)
+    index_size = len(index_data)
+    with open(os.path.join(outdir, "nvlasar_index.json"), "w") as fp:
+        json.dump(index_json, fp, indent=2)
+
+    nvlasar_entries = get_nvlasar_list(index_json)
+    nvlasar_map = get_nvlasar_hashmap(m, index_size + 0x10, nvlasar_entries)
+    dump_nvlasar_entries(m, index_size + 0x10, 
+        nvlasar_entries, nvlasar_map, outdir)
 
     os.close(fd)
 
 def debug():
+    export_nvlasar(r"D:\Download\tmp\game.asar", r"D:\Download\tmp\out2")
     pass
 
 def main():
     if len(sys.argv) < 3:
         print("nvl_asar e(export) inpath [ourdir]")
         return
-    
+
     inpath = sys.argv[2]
     if sys.argv[1].lower() == 'e': 
         outdir = sys.argv[3] if len(sys.argv) >= 4 else 'out'
         if not os.path.exists(outdir): os.makedirs(outdir)
         export_nvlasar(inpath, outdir)
     else: raise ValueError(f"{sys.argv[1]} not support!")
-
 
 if __name__ == '__main__':
     # debug()

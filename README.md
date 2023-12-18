d616f25f6538fb8a121f74d4e1c6e82a1a1d1c90
devseed <devseed1@gmail.com>
2023-12-18 22:02:19 +0900

    add psptm2.py

--- README.md
+++ README.md
@@ -3,160 +3,30 @@
 Reverse Projects for Galgame.  
 Util tools are moved to [ReverseUtil](https://github.com/YuriSizuku/ReverseUtil).
 
-## (1) Cross galgame
+## (1) Console galgame  
 
-### onscripter
+* `psptm2.py`, encode or decode `tm2` format, support swizzle // 231218 added  
 
-* `extract_nt3.c`,  extract *.nt3 script  
-* `nscript_patch.c`, support gbk, redirect `*.dat`, `*.arc` file
+### koei
 
-### krkr
+> 金色のコルダ (psp)
 
-* `krkr_patch.c`, make `krkr_patch.dll` for changing locale and redirect CHSPATCH  
-* `krkr_sjis2utf16bom` , batch convert `sjis` files to `utf-16le-bom` format  
-
-> つばさの丘の姫王
-
-* `sdhime_xp3enc.cpp`,  make encrypted xp3 files  
-* `sdhime_krkrpatch`, chs localization support
-
-### artemis
-
-* `artemis_pf8.py`,  pf8 format archive pack and unpack  
-
-### tyrano
-
-* `tyrano_extractexe.c` extract tyrano build-in exe files  
-
-> Q-bit_キグルミキノコ (android)  
-
-* `qbit_text.py` export and import text for translation
-
-## (2) PC galgame
-
-### azsystem
-
-> ラムネ
-
-* `lamune_hook.js`, decrypt the `*.asb` and `*.tbl` files  
-* `lamune_patch.c`, semi-dynamic framework for chs localization
-* `lamune_asbtext.py`, export or import text to `*.asb` files
-
-### majiro
-
-> そらいろ // 230815 added  
-> ルリのかさね ～いもうと物語り // 230822 added  
-
-* `majiro_arc.py`, export and build majiro `*.arc` file
-* `majiro_mjo.py`,  decrypt `*.mjo` file,  `MajiroObjX1.000` to `MajiroObjV1.000`
-* `majiro_mjiltext.py`, export and import text from `*.mjil` file by `mjotool2`
-
-> Winter Polaris
-
-* `winterpolaris_hook.js`, dump `mjo` and analyze `majiro` in `winter polaris` game  
-* `winterpolaris_patch.c`, Majirov3 dynamic hook framework code example  
-
-### systemnnn
-
-* `systemnnn_patch.c`, patch sjis check, change font, redirect files, repalce `dwq` with png
-* `systemnnn_spt.py`, parser `spt` opcode, export and import text
-
-> 倭人異聞録～あさき、ゆめみし～
-
-### yuris
-
-> 越えざるは红い花 remaster
-
-* `akaihana_yurispatch.c`, yuris gbk support  
-
-> 3M_Marionettes
-
-* `yuris_patch.c`, universe yuris patch, tested by 3M_Marionettes  
-
-### advhd (willplus)
-
-> あやかしごはん (advhd v1)  
-> Blackish House (advhd v2)  
-> 華は短し、踊れよ乙女 (advhd v2)  
-
-* `advhd_patch.c`, gbk support and overide arc file  
-* `advhd_arcv1.py`, willplus advhd v1 arc pack or unpack  
-* `advhd_arcv2.py`, willplus advhd v2 arc pack or unpack  
-* `advhd_wsc.py`, willplus advhd v1 wsc text export or import  
-* `advhd_ws2.py`, willplus advhd v2 ws2 text export or import  
-* `advhd_pna.py`, willplus advhd pna export and import  
-
-### criware
-
-> 祝姫
-
-* `xtx_font.py`, `xtx` font decode and encode, for `祝姫`  
-* `iwaihime_pc_decrypt.c`,  `sn.bin` decode  
-
-### ig (innocent gray)
-
-* `redirect_ig.c`, redirect the files to `xxx_chs` for separate CHSPATCH, tested in `天ノ少女`　`Innocent Gray`  
-
-> 天ノ少女
-
-### hibiki
-
-> Natrual Vacation
-
-* `hibiki_text_ks.py`, export and import game text for ftext format
-* `hibiki_rename_picture.py`, rename all the picture name to crc32, to avoid sjis file name problem
-
-### livemaker
-
-> アイするキミの居場所
-
-* `aikimi_loader.c`, a loader to dynamic inject DLL to the game
-* `aikimi_patch.c`, patch the game dynamiclly to support `GBK` text
-
-### bruns
-
-> 解体挿入新書
-
-* `bruns_decrypt.c` , decrypt  `EENZ` file,  `解体挿入新書` tested  
-
-### ffa
-
-> 天巫女姫
-
-* `amanomiko_patch.c`, add new lzss support and gbk support
-* `AmaNoMikoHime_lzss.py`, parse lzss compress file with header
-* `AmaNoMikoHime_SO4.py`, export or import text in so4 files
-* `AmaNoMikoHime_PT1.py`, parse PT1 image file rgb24 format
-
-### exhibit
-
-> 空色の風琴
-
-* `sorairo_patch.c`, support for gbk enconding text in dll
-
-### nvl  
-
-> 我和她的世界末日  
-
-* `nvl_asar.py`, to decrypt the `game.asar` made by nvlcloud  
-
-## (3) Console galgame
-
-### nippon1
-
-> 夜廻3 (switch)
-
-* `ykcmp.py`, an implementation in python to parse ykcmp compression.  
-* `yomawari3_nltx.py`, deal with switch swizzle texture in nltx file.
-
-> 神々の悪戯 (psp)
-
+* `KiniroNoCorda_cdar.py`, parse cdvdar (type v2) structure  
 (comming soon ...)  
+* `KiniroNoCorda_eboot.py`, patch the eboot for chs support, extend the fontmap and font glphy memory  
+* `KiniroNoCorda_font.py`, parse 4bpp 16X16 font  
+(in processing ...)  
+* `KiniroNoCorda_eventdat.py`, parse event text, export and import  
 
-* `KamigamiNoAsobi_nispack.py`, export or import nispack  
-* `KamigamiNoAsobi_story.py`, export or import text in story.dat  
-* `KamigamiNoAsobi_font.py`, analyze the multi page font  
-* `KamigamiNoAsobi_txp.py`, export or import txp picture  
+> 遙かなる時空の中で (psp)
+
+* `Harukanaru_cdar.py`, parse cdvdar (type v4) structure  
+
+### konami
+
+> ときめきメモリアル Girl's Side: 3rd Story (psp)
+
+* `GS3_scriptEVSC.py`, parse EVSC opcode, export or import text  
 
 ### prototype
 
@@ -197,9 +67,9 @@ Util tools are moved to [ReverseUtil](https://github.com/YuriSizuku/ReverseUtil)
 
 ### gss (takuyo)
 
-> 月影の鎖 -錯乱パラノイア (psp, psv)
-
 * `gss_arc.cs`, for `月影の鎖 -錯乱パラノイア` PSP, PSV see, my [pull request](https://github.com/morkt/GARbro/pull/435) in my forked GARBRO  
+
+> 月影の鎖 -錯乱パラノイア (psp, psv)
 
 ### cycrose
 
@@ -210,6 +80,23 @@ Util tools are moved to [ReverseUtil](https://github.com/YuriSizuku/ReverseUtil)
 * `baranoki_psp_fontfnt.py`, `baranoki_psp_fontp.py`, tile font support  
 * `baranoki_psp_boot.py`, rebuild the boot for fixing size buffer  
 
+### nippon1  
+
+* `ykcmp.py`, an implementation in python to parse ykcmp compression.  
+
+> 夜廻3 (switch)
+
+* `yomawari3_nltx.py`, deal with switch swizzle texture in nltx file.
+
+> 神々の悪戯 (psp)
+
+(comming soon ...)  
+
+* `KamigamiNoAsobi_nispack.py`, export or import nispack  
+* `KamigamiNoAsobi_story.py`, export or import text in story.dat  
+* `KamigamiNoAsobi_font.py`, analyze the multi page font  
+* `KamigamiNoAsobi_txp.py`, export or import txp picture  
+
 ### if (idea factory)
 
 > Jewelic Nightmare (psp)
@@ -219,29 +106,145 @@ Util tools are moved to [ReverseUtil](https://github.com/YuriSizuku/ReverseUtil)
 * `Jewelic_UF.py`,  building the UF tile font,  for`Jewelic Nightmare (ジュエリック・ナイトメア)`  
 * `Jewelic_STCM2L.py`,  converting the `ftext` (by [bintext.py](https://github.com/YuriSizuku/ReverseUtil/blob/master/script/bintext.py)) to STCM2Ltool format (made by [STCM2L_import.py](https://github.com/Yggdrasill-Moe/Helheim/blob/master/%E5%8D%81%E9%AC%BC%E4%B9%8B%E7%BB%8A/STCM2L_import.py)),  for`Jewelic Nightmare (ジュエリック・ナイトメア)`  
 
-### konami
-
-> ときめきメモリアル Girl's Side: 3rd Story (psp)
-
-* `GS3_scriptEVSC.py`, parse EVSC opcode, export or import text  
-
-### koei
-
-> 金色のコルダ (psp)
-
-(in processing ...)  
-
-* `KiniroNoCorda_cdar.py`, parse cdvdar (type v2) structure  
-* `KiniroNoCorda_eboot.py`, patch the eboot for chs support, extend the fontmap and font glphy memory  
-* `KiniroNoCorda_eventdat.py`, parse event text, export and import  
-* `KiniroNoCorda_font.py`, parse 4bpp 16X16 font  
-
-> 遙かなる時空の中で (psp)
-
-* `Harukanaru_cdar.py`, parse cdvdar (type v4) structure  
-
 ### entergram  
 
 > 9 Nine (switch)  // added on 231211  
 
 * `9nine_fnt.py`, extract and insert glphys for fnt font
+
+## (2) PC galgame
+
+### majiro
+
+* `majiro_arc.py`, export and build majiro `*.arc` file
+* `majiro_mjo.py`,  decrypt `*.mjo` file,  `MajiroObjX1.000` to `MajiroObjV1.000`
+* `majiro_mjiltext.py`, export and import text from `*.mjil` file by `mjotool2`
+
+> そらいろ // 230815 added  
+> ルリのかさね ～いもうと物語り // 230822 added  
+  
+> Winter Polaris
+
+* `winterpolaris_hook.js`, dump `mjo` and analyze `majiro` in `winter polaris` game  
+* `winterpolaris_patch.c`, Majirov3 dynamic hook framework code example  
+
+### azsystem
+
+> ラムネ
+
+* `lamune_hook.js`, decrypt the `*.asb` and `*.tbl` files  
+* `lamune_patch.c`, semi-dynamic framework for chs localization
+* `lamune_asbtext.py`, export or import text to `*.asb` files
+
+### systemnnn
+
+* `systemnnn_patch.c`, patch sjis check, change font, redirect files, repalce `dwq` with png
+* `systemnnn_spt.py`, parser `spt` opcode, export and import text
+
+> 倭人異聞録～あさき、ゆめみし～
+
+### advhd (willplus)
+
+* `advhd_patch.c`, gbk support and overide arc file  
+* `advhd_arcv1.py`, willplus advhd v1 arc pack or unpack  
+* `advhd_arcv2.py`, willplus advhd v2 arc pack or unpack  
+* `advhd_wsc.py`, willplus advhd v1 wsc text export or import  
+* `advhd_ws2.py`, willplus advhd v2 ws2 text export or import  
+* `advhd_pna.py`, willplus advhd pna export and import  
+
+> あやかしごはん (advhd v1)  
+> Blackish House (advhd v2)  
+> 華は短し、踊れよ乙女 (advhd v2)  
+
+### yuris  
+
+* `yuris_patch.c`, universe yuris patch, tested by 3M_Marionettes  
+
+> 越えざるは红い花 remaster
+
+* `akaihana_yurispatch.c`, yuris gbk support  
+
+> 3M_Marionettes
+
+### ffa
+
+> 天巫女姫
+
+* `amanomiko_patch.c`, add new lzss support and gbk support
+* `AmaNoMikoHime_lzss.py`, parse lzss compress file with header
+* `AmaNoMikoHime_SO4.py`, export or import text in so4 files
+* `AmaNoMikoHime_PT1.py`, parse PT1 image file rgb24 format
+
+### nvl  
+
+> 我和她的世界末日  
+
+* `nvl_asar.py`, to decrypt the `game.asar` made by nvlcloud  
+
+### criware
+
+> 祝姫
+
+* `xtx_font.py`, `xtx` font decode and encode, for `祝姫`  
+* `iwaihime_pc_decrypt.c`,  `sn.bin` decode  
+
+### hibiki
+
+> Natrual Vacation
+
+* `hibiki_text_ks.py`, export and import game text for ftext format
+* `hibiki_rename_picture.py`, rename all the picture name to crc32, to avoid sjis file name problem
+
+### livemaker
+
+> アイするキミの居場所
+
+* `aikimi_loader.c`, a loader to dynamic inject DLL to the game
+* `aikimi_patch.c`, patch the game dynamiclly to support `GBK` text
+
+### bruns
+
+> 解体挿入新書
+
+* `bruns_decrypt.c` , decrypt  `EENZ` file,  `解体挿入新書` tested  
+
+### ig (innocent gray)
+
+* `redirect_ig.c`, redirect the files to `xxx_chs` for separate CHSPATCH, tested in `天ノ少女`　`Innocent Gray`  
+
+> 天ノ少女
+
+### exhibit
+
+> 空色の風琴
+
+* `sorairo_patch.c`, support for gbk enconding text in dll
+
+## (3) Cross galgame
+
+### onscripter
+
+* `extract_nt3.c`,  extract *.nt3 script  
+* `nscript_patch.c`, support gbk, redirect `*.dat`, `*.arc` file
+
+### krkr
+
+* `krkr_patch.c`, make `krkr_patch.dll` for changing locale and redirect CHSPATCH  
+* `krkr_sjis2utf16bom` , batch convert `sjis` files to `utf-16le-bom` format  
+
+> つばさの丘の姫王
+
+* `sdhime_xp3enc.cpp`,  make encrypted xp3 files  
+* `sdhime_krkrpatch`, chs localization support
+
+### artemis
+
+* `artemis_pf8.py`,  pf8 format archive pack and unpack  
+
+### tyrano
+
+* `tyrano_extractexe.c` extract tyrano build-in exe files  
+
+> Q-bit_キグルミキノコ (android)  
+
+* `qbit_text.py` export and import text for translation
---
+++ project/koei/src/psptm2.py
@@ -0,0 +1,323 @@
+"""
+  for decoding and encoding tm2 format texture
+    v0.3, developed by devseed
+
+    tested games:
+    ULJM05054 金色のコルダ
+
+    refer:
+    https://openkh.dev/common/tm2.html
+    https://github.com/marco-calautti/Rainbow
+    
+"""
+
+import os
+import sys
+import numpy as np
+from enum import Enum
+from ctypes import *
+from PIL import Image, ImagePalette
+from typing import Tuple, List
+
+class COLOR_TYPE(Enum):
+    UNDEFINED = 0
+    A1B5G5R5 = 1
+    X8B8G8R8 = 2
+    A8B8G8R8 = 3
+    INDEX4 = 4
+    INDEX8 = 5
+
+class tm2pic_t(Structure):
+    _fields_ = [
+        ('size_total', c_uint32), # size stored in file
+        ('size_palette', c_uint32),  # clut size palette, palette is at the end
+        ('size_image', c_uint32), # bitmap size
+        ('size_header', c_ushort),
+        ('count_color', c_ushort), # used by clut
+        ('format', c_ubyte),
+        ('count_mipmap', c_ubyte),
+        ('type_clutcolor', c_ubyte),
+        ('type_imagecolor', c_ubyte), # IMAGE_COLOR_TYPE
+        ('width', c_ushort),
+        ('height', c_ushort),
+        ('reg_gstex', c_uint64 * 2),
+        ('reg_gsflag', c_uint32),
+        ('reg_gsclut', c_uint32),
+        ('picdata', c_byte * 1)
+    ]
+
+class tm2_t(Structure):
+    _fields_ = [
+        ('magic', c_char * 4), # TIM2
+        ('version', c_ubyte),  # 4
+        ('format', c_ubyte),   # 0
+        ('count', c_ushort),
+        ('reserved', c_int * 2),
+        ('pictures', tm2pic_t * 1)
+    ]
+
+class Tm2():
+    @classmethod
+    def swizzle(cls, x, y, w, tilew=32, tileh=8) -> Tuple[int, int]:
+        """
+        tilew 32 for 4bpp, 16 for higher than 8bpp
+        0 1 2 3     0 1 4 5 
+        4 5 6 7  -> 2 3 6 7
+        """
+
+        idx = x + w*y
+        tilesize = tilew * tileh
+        tileline = w // tilew
+        tileidx = idx // tilesize
+        tiley = tileidx // tileline
+        tilex = tileidx % tileline
+
+        tileinneridx = idx % tilesize
+        tileinnery = tileinneridx // tilew
+        tileinnerx = tileinneridx % tilew
+
+        x2 = tilex * tilew + tileinnerx
+        y2 = tiley * tileh + tileinnery
+        return (x2, y2)
+    
+    @classmethod
+    def idx2xy(cls, idx, w) -> Tuple[int, int]:
+        return (idx%w, idx//w)
+    
+    @classmethod
+    def xy2idx(cls, x, y, w) -> int:
+        return x + y*w
+
+    @classmethod
+    def deswizzle_img(cls, imgin: np.ndarray, tilew=32, tileh=8) -> np.ndarray:
+        """
+        deswizzle in image level
+        img h, w must be devided by tilew, tileh
+        """
+        
+        imgout = np.zeros_like(imgin)
+        (h, w) = imgout.shape[:2]
+        for y in range(h):
+            for x in range(w):
+                (x2, y2) = cls.swizzle(x, y, w, tilew, tileh)
+                imgout[y2, x2] = imgin[y, x]
+        return imgout
+
+    @classmethod
+    def swizzle_img(cls, imgin: np.ndarray, tilew=32, tileh=8) -> np.ndarray:
+        """
+        swizzle in image level
+        img h, w must be devided by tilew, tileh
+        """
+                
+        imgout = np.zeros_like(imgin)
+        (h, w) = imgin.shape[:2]
+        for y in range(h):
+            for x in range(w):
+                (x2, y2) = cls.swizzle(x, y, w, tilew, tileh)
+                imgout[y, x] = imgin[y2, x2]
+        return imgout
+
+    def __init__(self, data=None) -> None:
+        if data: self.parse(data)
+
+    def parse(self, data: bytearray):
+        self.m_data = data
+        self.tm2 = tm2_t.from_buffer(data)
+        if self.tm2.magic != b'TIM2':
+            raise ValueError(f"unknow magic {self.tm2.magic}")
+        
+        offset = addressof(self.tm2.pictures) - addressof(self.tm2)
+        self.tm2pics = [tm2pic_t.from_buffer(data, offset)]
+        for _ in range(self.tm2.count-1):
+            offset += self.tm2pics[-1].size_total
+            self.tm2pics.append(tm2pic_t.from_buffer(data, offset))
+
+    def get_picdata_offset(self, pic):
+        return addressof(pic) + pic.size_header - addressof(self.tm2) 
+
+    def decode_img(self, pic: tm2pic_t) -> Tuple[np.ndarray, List[np.ndarray]]:
+        """
+        decode single image from tm2 data
+        """
+
+        palette = None
+        w, h = pic.width, pic.height
+        offset_picdata = self.get_picdata_offset(pic)
+        color_type = COLOR_TYPE(pic.type_imagecolor)
+        if color_type == COLOR_TYPE.INDEX4 or \
+            color_type == COLOR_TYPE.INDEX8 :
+            size_palette = pic.size_palette # load palette
+            offset_palette = offset_picdata + pic.size_image # palette is at the end
+            palette = [np.frombuffer(self.m_data, np.uint8, 4, cur) 
+                       for cur in range(offset_palette, offset_palette + size_palette, 4)]
+            img = np.zeros((h, w, 4), dtype=np.uint8) # load image
+            for y in range(h):
+                for x in range(w):
+                    pos = x + w*y
+                    if color_type == COLOR_TYPE.INDEX4:
+                        d = self.m_data[offset_picdata + pos//2]
+                        d = d & 0xf if pos%2==0 else (d >> 4) & 0xf
+                    elif color_type == COLOR_TYPE.INDEX8:
+                        d = self.m_data[offset_picdata + pos]
+                    img[y, x] = palette[d]
+        elif color_type == COLOR_TYPE.A8B8G8R8:
+            img = np.frombuffer(self.m_data, 
+                    dtype=np.uint8, cout=pic.size_image, 
+                    offset=offset_picdata).resize((h, w, 4))
+        else: raise NotImplementedError(f"not support image color type {color_type}")
+
+        return img, palette
+
+    def encode_img(self, pic: tm2pic_t, img: np.ndarray, palette: List[np.ndarray]) -> tm2pic_t:
+        """
+        encode single image to tm2 data
+        """
+
+        h, w = img.shape[:2]
+        color_type = COLOR_TYPE(pic.type_imagecolor)
+        ppicdata = cast(c_void_p(addressof(pic.picdata)), POINTER(c_ubyte))
+        if color_type == COLOR_TYPE.INDEX4 or \
+            color_type == COLOR_TYPE.INDEX8:
+            palettedata = np.array(palette).tobytes()
+            assert(len(palettedata) == pic.size_palette)
+            ppalette = c_void_p(addressof(pic.picdata) + pic.size_image)
+            if color_type == COLOR_TYPE.INDEX4:
+                assert(img.size == 2*pic.size_image)
+                for i in range(pic.size_image):
+                    low, high = img[2*i//w, 2*i%w], img[(2*i+1)//w, (2*i+1)%w]
+                    ppicdata[i] = (low&0xf) + ((high<<4)&0xf0)
+            elif color_type == COLOR_TYPE.INDEX8:
+                assert(img.size == pic.size_image)
+                memmove(ppicdata, img.tobytes(), pic.size_image)
+            memmove(ppalette, palettedata, pic.size_palette)
+        elif color_type == COLOR_TYPE.A8B8G8R8:
+            assert(img.size == pic.size_image)
+            memmove(ppicdata, img.tobytes(), pic.size_image)
+        else: raise NotImplementedError(f"not support image color type {color_type}")
+
+    def extract(self, idx, outpath="out.png", use_swizzle=False):
+        if idx > self.tm2.count or idx < 0: return None
+        pic = self.tm2pics[idx]
+        if(pic.count_mipmap > 1): raise NotImplementedError(f"not support mipmap {pic.count_mipmap}")
+        
+        img, palette = self.decode_img(pic)
+        if use_swizzle:
+            tilew, tileh = 16, 8
+            if len(palette)==16: tilew=32
+            img = self.deswizzle_img(img, tilew, tileh)
+
+        if outpath != "":
+            imgpil = Image.fromarray(np.array(img)) 
+            if palette and 0: # not extract with palatte, this makes alpha not work
+                palettepil = ImagePalette.ImagePalette("RGBA", np.array(palette).tobytes())
+                imgpil = imgpil.convert("P", palette=palettepil, colors=len(palette)//2)
+            imgpil.save(outpath)  
+        return img
+
+    def insert(self, idx, inpath="", use_swizzle=False):
+        if idx > self.tm2.count or idx < 0: return None
+        pic = self.tm2pics[idx]
+        if(pic.count_mipmap > 1): raise NotImplementedError(f"not support mipmap {pic.count_mipmap}")
+        
+        palette = None
+        imgpil = Image.open(inpath)
+        if pic.type_imagecolor == COLOR_TYPE.INDEX4.value:
+            imgpil = imgpil.convert("P", colors=16)
+            palette = [np.array(color, np.uint8) for color in imgpil.palette.colors]
+        elif pic.type_imagecolor == COLOR_TYPE.INDEX8.value:
+            imgpil = imgpil.convert("P", colors=256)
+            palette = [np.array(color, np.uint8) for color in imgpil.palette.colors]
+        img = np.array(imgpil)
+ 
+        if use_swizzle:
+            tilew, tileh = 16, 8
+            if len(palette)==16: tilew=32
+            img = self.swizzle_img(img, tilew, tileh)
+        
+        self.encode_img(pic, img, palette)
+        self.parse(self.m_data) 
+
+def extract_tm2(tm2path, outdir="out", use_swizzle=False):
+    with open(tm2path, 'rb') as fp:
+        data = bytearray(fp.read())
+    tm2 = Tm2(data)
+    for i in range(tm2.tm2.count):
+        name = os.path.basename(os.path.splitext(tm2path)[0])
+        path = os.path.join(outdir, f"{name}_{i}.png")
+        tm2.extract(i, path, use_swizzle)
+        print(f"{i+1}/{tm2.tm2.count}: {path} extracted")
+
+def insert_tm2(tm2path, indir, outpath="out.tm2", use_swizzle=False):
+    with open(tm2path, 'rb') as fp:
+        data = bytearray(fp.read())
+    tm2 = Tm2(data)
+    for i in range(tm2.tm2.count):
+        name = os.path.basename(os.path.splitext(tm2path)[0])
+        path = os.path.join(indir, f"{name}_{i}.png")
+        if os.path.exists(path):
+            tm2.insert(i, path, use_swizzle)
+            print(f"{i+1}/{tm2.tm2.count}: {outpath} inserted")
+    with open(outpath, 'wb') as fp:
+        fp.write(data)
+
+def debug():
+    pass
+
+def cli(argv):
+    def cmd_help():
+        print("psptm2 tools v0.3, developed by devseed")
+        print("usage")
+        print("psptm2 e[swi] tm2path [outdir] # convert tm2 to pngs")
+        print("psptm2 i[swi] tm2path indir [outpath] # insert pngs to tm2")
+        print("psptm2 c[swi|deswi][tilew|tileh] imgpath [outpath] # swizzle or deswizzle image")
+        print("examples: ")
+        print("psptm2 eswi xxx.tm2 ./")
+        print("psptm2 iswi xxx.tm2 ./ xxx_rebuild.tm2")
+        print("psptm2 cdeswi1608 xxx.png xxx_deswi.png")
+
+    def cmd_export():
+        outdir = argv[3] if len(argv) > 3 else "out"
+        extract_tm2(inpath, outdir, use_swizzle)
+    
+    def cmd_import():
+        indir = argv[3]
+        outpath = argv[4] if len(argv) > 4 else "out.tm2"
+        insert_tm2(inpath, indir, outpath, use_swizzle)
+
+    def cmd_convert():
+        outpath = argv[3] if len(argv) > 3 else "out.png"
+        imgpil = Image.open(inpath)
+        tilew, tileh = 16, 8
+        if cmdtype[1:4] == 'swi':
+            if len(cmdtype) > 4: tilew = int(cmdtype[4:6], 10)
+            if len(cmdtype) > 6: tileh = int(cmdtype[6:8], 10)
+            img = Tm2.swizzle_img(np.array(imgpil), tilew, tileh)
+        elif cmdtype[1:6] == 'deswi':
+            if len(cmdtype) > 6: tilew = int(cmdtype[6:8], 10)
+            if len(cmdtype) > 8: tileh = int(cmdtype[8:10], 10)
+            img = Tm2.deswizzle_img(np.array(imgpil), tilew, tileh)
+        else: raise NotImplementedError(f"unknow type {cmdtype}")
+        imgpil2 = Image.fromarray(img, mode=imgpil.mode)
+        if imgpil.mode=='P': imgpil2.putpalette(imgpil.palette)
+        imgpil2.save(outpath)
+
+    if len(argv) < 3: cmd_help();return
+    cmdtype = argv[1].lower()
+    inpath = argv[2] 
+    use_swizzle  = True if cmdtype.find("swi") > 0 else False
+    if cmdtype[0] == 'e': cmd_export()
+    elif cmdtype[0] == 'i': cmd_import()
+    elif cmdtype[0] == 'c': cmd_convert()
+    else: raise NotImplementedError(f"unknow type {cmdtype}")
+
+if __name__=='__main__':
+    # debug()
+    cli(sys.argv)
+
+"""
+history:
+  v0.1, implement export iamge with COLOR_A8B8G8R8, COLOR_INDEX4, COLOR_INDEX8
+  v0.2, add swizzle deswizzle method
+  v0.3, add insert png32 to tim2, and convert with index4/index8 if necessory
+"""
